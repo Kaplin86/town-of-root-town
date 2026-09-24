1662782b07e237ef7562bf79349756c61d87ec47
Kaplin86 <87865073+Kaplin86@users.noreply.github.com>
2026-09-24 19:06:37 -0400

    wip worms

--- code/tile_manager.gd
+++ code/tile_manager.gd
@@ -3,38 +3,61 @@ extends Node
 const MAPSIZEX = 200
 const MAPSIZEY = 400
 
-
-
 var Tiles : Dictionary[Vector2i,String]
-@export var NoiseThreshold = 0.0
+
+var noise = FastNoiseLite.new()
+
+var activeWorms = []
+var wormID = 0
 
 func _generate():
-	var noise = FastNoiseLite.new()
-	noise.seed = 0
-	noise.noise_type = FastNoiseLite.TYPE_SIMPLEX
-	#noise.cellular_return_type = FastNoiseLite.RETURN_CELL_VALUE
-	noise.fractal_gain = 0.39
-	noise.frequency = 0.05
+	Tiles = {}
+	noise.noise_type = FastNoiseLite.TYPE_PERLIN
+	noise.frequency = 0.56
+	
 	for x in MAPSIZEX:
-		var halfwayPercent = inverse_lerp(0,MAPSIZEX,x) #for later
-		halfwayPercent -= 0.5 #for later
-		halfwayPercent = abs(halfwayPercent) - 0.2
-		
 		for y in MAPSIZEY:
-			var verticalPercent = inverse_lerp(0,MAPSIZEY,y)
-			
-			
-			if noise.get_noise_2d(x ,y * 0.8) >= NoiseThreshold + (halfwayPercent * 3) + (verticalPercent * 0.8):
-				Tiles[Vector2i(x,y)] = "Root"
-			else:
-				Tiles[Vector2i(x,y)] = "Dirt"
-	_display()
-	await get_tree().create_timer(0.2).timeout
+			Tiles[Vector2i(x,y)] = "Dirt"
+	
+	await doWorm(Vector2(100,0), 10, 90)
+	
+	while activeWorms != []:
+		await get_tree().create_timer(0.5).timeout
+	
 	_generate()
 	
 func _ready():
 	_generate()
 	
+
+func doWorm(startPos, thickness, angleOffset):
+	var myID = wormID
+	wormID += 1
+	activeWorms.append(myID)
+	var worm_pos: Vector2 = startPos
+	var worm_dist = randi_range(1,200 - (200 / thickness))
+	
+	for C in worm_dist:
+		if randi_range(0,10) == 5:
+			await RenderingServer.frame_post_draw
+		var angle = noise.get_noise_2d(worm_pos.x * 0.5, worm_pos.y) * PI * 0.8
+		angle += deg_to_rad(angleOffset)
+		angle += randf_range(-0.25, 0.25)
+		worm_pos += Vector2(cos(angle) * 1.2, sin(angle)) * 1.5
+		for I in thickness:
+			var offset = (I * 0.5) - thickness
+			Tiles[Vector2i(worm_pos) - Vector2i(offset,0)] = "Root"
+		
+		if randf_range(0,1) <= 0.1 and thickness > 2 and C > 30:
+			doWorm(worm_pos,thickness - 2,angleOffset + randf_range(-60,60))
+			if randf_range(0,1) < 0.5:
+				doWorm(worm_pos,thickness - 2,angleOffset + randf_range(-60,60))
+			thickness -= 1
+	
+	activeWorms.erase(myID)
+
+func _process(delta):
+	_display()
 
 func _display():
 	$"../CoveringGround".clear()
