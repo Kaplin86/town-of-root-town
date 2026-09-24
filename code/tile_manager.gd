5518e19bf437a39492c082216f9d22bb20879190
Kaplin86 <87865073+Kaplin86@users.noreply.github.com>
2026-09-24 19:41:20 -0400

    good roots 😇

--- code/tile_manager.gd
+++ code/tile_manager.gd
@@ -1,7 +1,7 @@
 extends Node
 
-const MAPSIZEX = 200
-const MAPSIZEY = 400
+const MAPSIZEX = 40
+const MAPSIZEY = 40
 
 var Tiles : Dictionary[Vector2i,String]
 
@@ -19,11 +19,12 @@ func _generate():
 		for y in MAPSIZEY:
 			Tiles[Vector2i(x,y)] = "Dirt"
 	
-	await doWorm(Vector2(100,0), 10, 90)
+	await doWorm(Vector2(20,0), 4, 90)
 	
 	while activeWorms != []:
 		await get_tree().create_timer(0.5).timeout
 	
+	await get_tree().create_timer(0.3).timeout
 	_generate()
 	
 func _ready():
@@ -35,7 +36,13 @@ func doWorm(startPos, thickness, angleOffset):
 	wormID += 1
 	activeWorms.append(myID)
 	var worm_pos: Vector2 = startPos
-	var worm_dist = randi_range(1,200 - (200 / thickness))
+	var worm_dist = {
+		4: round((18.0/40.0) * MAPSIZEY),
+		3: round((10.0/40.0) * MAPSIZEY),
+		2: round((5.0/40.0) * MAPSIZEY),
+		1: round((5.0/40.0) * MAPSIZEY),
+		0: 0,
+	}[thickness]
 	
 	for C in worm_dist:
 		if randi_range(0,10) == 5:
@@ -43,15 +50,27 @@ func doWorm(startPos, thickness, angleOffset):
 		var angle = noise.get_noise_2d(worm_pos.x * 0.5, worm_pos.y) * PI * 0.8
 		angle += deg_to_rad(angleOffset)
 		angle += randf_range(-0.25, 0.25)
-		worm_pos += Vector2(cos(angle) * 1.2, sin(angle)) * 1.5
+		worm_pos += Vector2(cos(angle) * 1.2, sin(angle)) * 1
 		for I in thickness:
-			var offset = (I * 0.5) - thickness
-			Tiles[Vector2i(worm_pos) - Vector2i(offset,0)] = "Root"
+			var offset = I - (thickness * 0.5)
+			Tiles[Vector2i(worm_pos) + Vector2i(offset,0)] = "Root"
 		
-		if randf_range(0,1) <= 0.1 and thickness > 2 and C > 30:
-			doWorm(worm_pos,thickness - 2,angleOffset + randf_range(-60,60))
-			if randf_range(0,1) < 0.5:
-				doWorm(worm_pos,thickness - 2,angleOffset + randf_range(-60,60))
+		if thickness == 4 and C == round(worm_dist / 2.0):
+			var sideOffset
+			if randi_range(0,1) == 1:
+				sideOffset = randf_range(-60,-30)
+			else:
+				sideOffset = randf_range(30,60)
+			doWorm(worm_pos,thickness - 1,angleOffset + sideOffset)
+		
+		if C == worm_dist - 1:
+			var sideOffset
+			if randi_range(0,1) == 1:
+				sideOffset = randf_range(-60,-30)
+			else:
+				sideOffset = randf_range(30,60)
+			doWorm(worm_pos,thickness - 1,angleOffset + sideOffset)
+			doWorm(worm_pos,thickness - 1,angleOffset + randf_range(-60,60))
 			thickness -= 1
 	
 	activeWorms.erase(myID)
